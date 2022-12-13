46fe83cf405b4a6f196937f13eabc36ae3374910
Rafael <rafa.trevi@hotmail.com>
2022-12-13 20:42:16 -0300

    init repo

---
+++ steel/testa-padrao/sim/rtl/file_list.f
@@ -0,0 +1,4 @@
+-smartorder -ieee1364 -work work -notimingchecks -gui -access +rw
+../../rtl/riscv_steel_core.v
+../tb/tb_riscv_steel_core.v
+
---
+++ steel/testa-padrao/sim/sim-steel-1/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,2 @@
+1662626486 /home/inf01185/rafael.trevisan/CCI2/CCI2/steel/testa-padrao/rtl/riscv_steel_core.v
+1662626485 /home/inf01185/rafael.trevisan/CCI2/CCI2/steel/testa-padrao/sim/tb/tb_riscv_steel_core.v
