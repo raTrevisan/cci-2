0b716fa2607ed35e514c75455ded4f471a8e9039
Rafael Trevisan <rafa.trevi@hotmail.com>
2022-12-13 22:28:42 -0300

    tb tests unautomated

--- steel/sim/tb/tb_riscv_steel_core.v
+++ steel/sim/tb/tb_riscv_steel_core.v
@@ -192,10 +192,33 @@ module tb_riscv_steel_core();
       for(i = 0; i < 16384; i=i+1) ram[i] = 32'b0;
       // Clear signature
       for(i = 0; i < 2048;   i=i+1) current_test_goldenref[i] = 32'b0;
-      // Load test program into RAM
-      $readmemh("../tb/add-01.mem",ram);      
-      // Load reference signature for this test
-      $readmemh("../tb/add-01.reference.mem",current_test_goldenref);   
+
+      // Load test program into RAM "ADD" and its reference signature to ram
+      if (k == 0) begin
+        $readmemh("../tb/add-01.mem",ram);      
+        $readmemh("../tb/add-01.reference.mem",current_test_goldenref);
+      end
+      // Load test program into RAM "SUB" and its reference signature to ram
+      if (k == 1) begin
+        $readmemh("../tb/sub-01.mem",ram);      
+        $readmemh("../tb/sub-01.reference.mem",current_test_goldenref);
+      end   
+      // Load test program into RAM "XOR" and its reference signature to ram
+      if (k == 2) begin
+        $readmemh("../tb/xor-01.mem",ram);      
+        $readmemh("../tb/xor-01.reference.mem",current_test_goldenref);
+      end
+      // Load test program into RAM "BNE" and its reference signature to ram
+      if (k == 3) begin
+        $readmemh("../tb/bne-01.mem",ram);      
+        $readmemh("../tb/bne-01.reference.mem",current_test_goldenref);
+      end   
+      // Load test program into RAM "LUI" and its reference signature to ram
+      if (k == 4) begin
+        $readmemh("../tb/lui-01.mem",ram);      
+        $readmemh("../tb/lui-01.reference.mem",current_test_goldenref);
+      end
+             
       // Execution is aborted if j reaches 500000 cycles (~1ms)
       for(j = 0; j < 500000; j=j+1) begin
         #20;
